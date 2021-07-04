2d4e15831e0d328923ae84808f93844c9f028633
RahulSachdev27 <rahulsachdev250@gmail.com>
2021-07-04 18:16:27 +0800

    test

---
+++ AdvisoryAppsTest/Assets/Scripts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public GameObject ObjectB;
+    public float speed;
+    void Update()
+    {
+        Rotation();
+    }
+
+    void Rotation()
+    {
+        transform.RotateAround(ObjectB.transform.position, Vector3.up, speed * Time.deltaTime);
+    }
+}
